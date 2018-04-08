4ec005d476e4d5cf507d6e022edcf7357e54cc7d
rudi8848 <anna.v8848@gmail.com>
2018-04-09 01:58:29 +0300

    start

---
+++ minishell.h
@@ -0,0 +1,14 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <unistd.h>		//fork, execve, chdir,access, getcwd
+#include <signal.h>		//signal, kill
+#include <sys/types.h>		//wait, fork
+#include <sys/wait.h>		//wait
+#include <sys/time.h>		//
+#include <sys/resource.h>	//wait3, wait4
+#include <stdlib.h>		//exit
+#include <errno.h>
+
+
+#endif
